dbe128fb27e237563d9320be252f2604a50ec885
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-27 18:25:32 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(collection)
   i = 0
 
   while i < collection.length
-    yield(collection[i])
+    new_collection = yield(collection[i])
     i += 1
   end
-  collection 
+  collection
 end
